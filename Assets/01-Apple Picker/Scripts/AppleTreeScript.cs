c39aa9769645e834a7d7be8ffd984e8680598913
d00dledan <djc382@scarletmail.rutgers.edu>
2022-09-22 12:59:02 -0400

    Update AppleTreeScript.cs
    tree now cant go out of bounds

--- Assets/01-Apple Picker/Scripts/AppleTreeScript.cs
+++ Assets/01-Apple Picker/Scripts/AppleTreeScript.cs
@@ -24,5 +24,14 @@ public class AppleTreeScript : MonoBehaviour
         Vector3 pos = transform.position;
         pos.x += speed * Time.deltaTime;
         transform.position = pos;
+
+        if (pos.x < -leftAndRightEdge)
+        {
+            speed = Mathf.Abs(speed);
+        }
+        else if (pos.x > leftAndRightEdge) { 
+            speed = -Mathf.Abs(speed);  
+        }
+
     }
 }
